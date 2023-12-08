66ee437470c0ab73ef28788cca7f0ef83dcf3d6e
Bob Loblaw <b0bl0blawslawbl0g@proton.me>
2023-12-07 17:02:58 -0700

    Retrieve and send metadata token

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -9,7 +9,8 @@ if [ ! -z "$AS_FARGATE" ] && [ "$AS_FARGATE" = true ]; then
   ENI=$(echo $TASK_DETAILS | jq -r '.[] | select(.name=="networkInterfaceId").value')
   PUBLIC_IP=$(aws ec2 describe-network-interfaces --network-interface-ids "${ENI}" --query 'NetworkInterfaces[0].Association.PublicIp' --output text)
 else:
-  PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+  TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+  PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 fi
 
 echo "Using public IP address: ${PUBLIC_IP}"
--- .docker/lokinet-router/entrypoint.sh
+++ .docker/lokinet-router/entrypoint.sh
@@ -12,8 +12,9 @@ if [ ! -z "$AS_FARGATE" ] && [ "$AS_FARGATE" = true ]; then
   export PUBLIC_IP=$(aws ec2 describe-network-interfaces --network-interface-ids "${ENI}" --query 'NetworkInterfaces[0].Association.PublicIp' --output text)
   export PRIVATE_IP=$(aws ec2 describe-network-interfaces --network-interface-ids "${ENI}" --query 'NetworkInterfaces[0].PrivateIpAddress' --output text)
 else:
-  export PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-  export PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+  TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+  export PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+  export PRIVATE_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
 fi
 
 echo "Using public IP address: ${PUBLIC_IP}"
