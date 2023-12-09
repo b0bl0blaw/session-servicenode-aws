b60f8cb93efe4257a41114fed1b59019c7f00c9e
Bob Loblaw <b0bl0blawslawbl0g@proton.me>
2023-12-08 19:26:03 -0700

    Add shellcheck suggestions

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
-# yes | oxend-download-lmdb https://public.loki.foundation/loki/data.mdb
-
-if [ ! -z "$AS_FARGATE" ] && [ "$AS_FARGATE" = true ]; then
+if [ -n "$AS_FARGATE" ] && [ "$AS_FARGATE" = true ]; then
   TASK_ARN=$(aws ecs list-tasks --cluster "$ECS_CLUSTER_NAME" --service-name "$ECS_SERVICE_NAME" --query 'taskArns[0]' --output text)
   TASK_DETAILS=$(aws ecs describe-tasks --cluster "$ECS_CLUSTER_NAME" --task "${TASK_ARN}" --query 'tasks[0].attachments[0].details')
-  ENI=$(echo $TASK_DETAILS | jq -r '.[] | select(.name=="networkInterfaceId").value')
+  ENI=$(echo "$TASK_DETAILS" | jq -r '.[] | select(.name=="networkInterfaceId").value')
   PUBLIC_IP=$(aws ec2 describe-network-interfaces --network-interface-ids "${ENI}" --query 'NetworkInterfaces[0].Association.PublicIp' --output text)
 else
   TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
@@ -15,4 +13,4 @@ fi
 
 echo "Using public IP address: ${PUBLIC_IP}"
 
-exec "$@" --service-node-public-ip ${PUBLIC_IP}
+exec "$@" --service-node-public-ip "${PUBLIC_IP}"
